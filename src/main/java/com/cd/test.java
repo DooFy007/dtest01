02f88bef184558f2a43b4a6fec57f2121aaf721b
DooFy007 <2516440443@qq.com>
2020-04-13 12:26:15 +0800

    hello world

---
+++ src/main/java/com/cd/test.java
@@ -0,0 +1,14 @@
+package com.cd;
+
+/**
+ * @ClassName HelloWorld
+ * @Description:
+ * @Author DooFy
+ * @Date 2020/4/13
+ * @Version
+ **/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
